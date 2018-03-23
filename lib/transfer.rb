364fe417e4d4e81a4647c708766251f7b0db1090
Trevor Seitz <trevinla@gmail.com>
2018-03-23 19:02:44 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,12 @@
 class Transfer
-  # your code here
+  attr_accessor :balance, :status, :amount, :name_to, :name_from
+  attr_reader
+
+  def initialize(name_to, name_from, amount)
+    @name_to = name_to
+    @name_from = name_from
+    @amount = amount
+    @balance = 1000
+    @status = "open"
+  end
 end
