91dc35b51594408a613b1e1463868e51c6f4b68d
Trevor Seitz <trevinla@gmail.com>
2018-03-23 19:38:15 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
       receiver.balance += amount
       @status = "complete"
     else
-      @status = "rejected"
+    @status = "rejected"
     "Transaction rejected. Please check your account balance."
         # binding.pry
     end
